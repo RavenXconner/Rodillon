16d65b3caf8d4a5ff8a571add4361237379ef2ba
Javy Rodillon <reyfoxconner@gmail.com>
2025-03-03 21:06:32 +0800

    Remove unused files and directories from the project

--- javy/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Vehicle, Booking, Payment, Review, LoyaltyProgram
-
-admin.site.register(Vehicle)
-admin.site.register(Booking)
-admin.site.register(Payment)
-admin.site.register(Review)
-admin.site.register(LoyaltyProgram)
